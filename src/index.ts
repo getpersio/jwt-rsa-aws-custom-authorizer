7dcb425558896edd6a8250a02baa36181037e16e
David Papp <david.papp@whenthen.com>
2022-09-23 13:45:14 +0200

    :tada: Add AWS Xray support

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
 import { authenticate } from './lib';
+import AWSXRay from 'aws-xray-sdk-core';
+
+const AWS = AWSXRay.captureAWS(require('aws-sdk'));
+AWSXRay.setContextMissingStrategy(() => { });
+// const lambda = new AWS.Lambda();
+
 let data: any;
 
 // Lambda function index.handler - thin wrapper around lib.authenticate
